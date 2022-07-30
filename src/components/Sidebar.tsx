1240411b790eea37d3f2845fc0ca6ad36c5f2948
ecthon <ecthon@gmail.com>
2022-07-29 23:52:20 -0400

    Estrutura da aplicação - Componente: Avatar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,7 +10,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1618209076877-3b577b275eef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGhhcmR3YXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/ecthon.png" />
+                <Avatar src="https://github.com/ecthon.png" />
                 <strong>Ecthon Almeida</strong>
                 <span>Web Developer</span>
             </div>
