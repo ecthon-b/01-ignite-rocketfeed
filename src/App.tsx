96f0d01bc461d450ce27b4e29d18faa480314db6
ecthon <ecthon@gmail.com>
2022-07-30 21:34:00 -0400

    Criando array de posts

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,43 @@ import { Sidebar } from "./components/Sidebar";
 import './global.css';
 import styles from './App.module.css';
 
+// author: {avatar_url:"", name:"", role:""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https:// github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', conente: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-07-07 22:00:00'),
+
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/ecthon.png',
+      name: 'Ecthon Borhis',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', conente: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-07-07 07:00:00'),
+
+  },
+];
+
 export function App() {
   return (
     <>
@@ -13,10 +50,18 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </>
   )
 }
+
 
