d55d2b76b46f1f7e627f1c40d37ca9302b62c9c7
ecthon <ecthon@gmail.com>
2022-08-02 19:57:30 -0400

    Os motores do React - Comunicação entre componentes.

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -34,6 +34,10 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(comment) {
+
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -74,7 +78,13 @@ export function Post({ author, publishedAt, content }) {
             </form>
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment} />
+                    return (
+                        <Comment
+                            key={comment}
+                            content={comment}
+                            onDeleteComment={deleteComment}
+                        />
+                    )
                 })}
             </div>
         </article>
